7e213fd327964d1aafe5557c302a5d4d4630d993
nurez305 <nurudeen.saliu@decagon.dev>
2023-04-26 06:46:35 +0100

    added my blog website

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <a href="https://nurez-blog.vercel.app/" className='btn btn-primary'>Visit My Blog</a>
     </div>
   )
 }
