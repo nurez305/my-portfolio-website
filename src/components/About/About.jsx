be5f395f3e96281d033650bf499656f5702c2897
nurez305 <nurudeen.saliu@decagon.dev>
2023-11-01 10:49:02 +0100

    updating-about-message

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ import ME from "../../assets/nurez.jpeg";
 import { FaAward } from "react-icons/fa";
 import { FaUserTie } from "react-icons/fa";
 import { FaRegFolderOpen } from "react-icons/fa";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 function About() {
   return (
@@ -13,20 +13,22 @@ function About() {
       <h2>About Me</h2>
 
       <div className="container about_container">
-        <motion.div className="about_me"
-        initial={{x: "-100vw"}}
-        animate={{x: 0}}
-        transition={{type: "spring", stiffness: 50}}
+        <motion.div
+          className="about_me"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "spring", stiffness: 50 }}
         >
           <div className="about_me-image">
             <img src={ME} alt="" />
           </div>
         </motion.div>
 
-        <motion.div className="about_content"
-        initial={{x: "100vw"}}
-        animate={{x: 0}}
-        transition={{type: "spring", stiffness: 50}}
+        <motion.div
+          className="about_content"
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "spring", stiffness: 50 }}
         >
           <div className="about_cards ">
             <article className="about_card">
@@ -48,23 +50,24 @@ function About() {
             </article>
           </div>
           <p>
-            I am a Full Stack Developer with over 4 years of
-            experience, specializing in NodeJS.
-            I have experience in developing and deploying websites utilizing
-            various open-source packages.I have extensive experience in HTML,
-            CSS, JavaScript, Typescript, NodeJS, ExpressJS, ReactJS, MongoDB,
-            Docker and Dockerhub, TDD with jest and Git and Github.<br /> <br /> 
-            I am skilled in using these frameworks to develop web
-            applications based on Single Page Applications (SPA) architectures.
-            I also possess skills in writing SQL queries using popular
-            database servers such as MySQL. In addition to my development work,
-            I am skilled in troubleshooting problems and resolving bugs within
-            an existing system.<br /> <br /> 
-            Other skills include Scrum, Agile Methodology, Jira, Communication,
-            Team Coordination. If it will solve a problem, I want to be
-            part of it. Just send a{" "}
+            I am a full-stack developer with over 4 years of experience,
+            specializing in Javascript, Typescript, and Flutter. I have
+            experience in developing and deploying websites utilizing various
+            open-source packages. I have extensive experience in HTML, CSS,
+            JavaScript, Typescript, NodeJS, ExpressJS, ReactJS, MongoDB, Docker
+            and Dockerhub, TDD with Jest, and Git and Github. <br /> <br /> 
+            I am skilled in
+            using these frameworks to develop web applications based on Single
+            Page Applications (SPA) architectures. I also possess skills in
+            writing SQL queries using popular database servers such as MySQL. In
+            addition to my development work, I am skilled at troubleshooting
+            problems and resolving bugs within an existing system. <br /> <br /> 
+            Other skills
+            include scrum, agile methodology, Jira, communication, and team
+            coordination. If it will solve a problem, I want to be part of it.
+            Just send a{" "}
             <span className="anchor_message">
-              <a href="#contact">Click To Message</a>
+              <a href="#contact">click-to-message</a>
             </span>
             . Solving problems with tech excites me. I am open to working with
             you.
